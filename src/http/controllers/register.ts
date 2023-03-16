e6eb8e3e3054774b57676c68a23044822fc077f5
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-03-15 21:34:51 -0300

    add: dependency inversion

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { registerUseCase } from '@/uses-cases/register'
+import { RegisterUseCase } from '@/uses-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
 	const registerBodySchema = z.object({
@@ -12,7 +13,10 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
 	const { name, email, password } = registerBodySchema.parse( req.body )
 
 	try {
-		await registerUseCase({ name, email, password })
+		const prismaUsersRepository = new PrismaUsersRepository()
+		const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+		await registerUseCase.execute({ name, email, password })
 	}
 	catch(err) {
 		return res.status(409).send()
