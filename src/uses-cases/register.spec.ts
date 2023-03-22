d304786d4d77fb0514f48607658a07baf763a210
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-03-21 22:09:14 -0300

    add: first unitary test and in-memory database pattern

--- src/uses-cases/register.spec.ts
+++ src/uses-cases/register.spec.ts
@@ -1,5 +1,24 @@
-import { expect, test } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { compare } from 'bcryptjs'
+import { expect, describe, it } from 'vitest'
+import { RegisterUseCase } from './register'
 
-test('check if it works', () => {
-	expect(2 + 2).toBe(4)
+describe('Register use case', () => {
+	it('should hash user password upon registration', async () => {
+		const usersRepository = new InMemoryUsersRepository()
+		const registerUseCase = new RegisterUseCase(usersRepository)
+
+		const { user } = await registerUseCase.execute({
+			name: 'John Doe',
+			email: 'john_doe@gmail.com',
+			password: '123456'
+		})
+
+		const isPasswordCorrectlyHashed = await compare(
+			'123456',
+			user.password_hash
+		)
+
+		expect(isPasswordCorrectlyHashed).toBe(true)
+	})
 })
--- src/uses-cases/register.ts
+++ src/uses-cases/register.ts
@@ -1,4 +1,5 @@
 import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
@@ -8,6 +9,10 @@ interface RegisterUseCaseRequest {
     password: string
 }
 
+interface RegisterUseCaseResponse {
+	user: User
+}
+
 export class RegisterUseCase {
 	private usersRepository: UsersRepository
 
@@ -15,7 +20,7 @@ export class RegisterUseCase {
 		this.usersRepository = usersRepository
 	}
 
-	async execute({ name, email, password }: RegisterUseCaseRequest) {
+	async execute({ name, email, password }: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
 		const password_hash = await hash(password, 6)
 	
 		const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -24,12 +29,15 @@ export class RegisterUseCase {
 			throw new UserAlreadyExistsError()
 		}
 	
-		await this.usersRepository.create({
+		const user = await this.usersRepository.create({
 			name,
 			email,
 			password_hash
 		})
 		
+		return { 
+			user,
+		}
 	}
 
 }
