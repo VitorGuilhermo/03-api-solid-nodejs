569f0a508e00609a1bcbb4f0e282e19450447b5a
Vitor Guilhermo <viguilhermo@hotmail.com>
2023-08-28 21:54:50 -0300

    add: use case check-ins history and get user metrics use case

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -21,6 +21,13 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 		return checkIn
 	}
 
+	countByUserId(userId: string): number {
+		return this.items
+			.filter(item => item.user_id === userId)
+			.length
+	}
+	
+
 	async findByUserIdOnDate(userId: string, date: Date) {
 		const startOfTheDay = dayjs(date).startOf('date')
 		const endOfTheDay = dayjs(date).endOf('date')
@@ -38,5 +45,11 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
 
 
 		return checkInOnSameDate
+	}
+
+	async findManyByUserId(userId: string, page: number){
+		return this.items
+			.filter(item => item.user_id === userId)
+			.slice((page - 1) * 20, page * 20)
 	}
 }
